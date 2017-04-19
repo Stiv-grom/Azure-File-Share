966e085c4ca39f34cd167ac4e90ab9ab7ea23cdb
stiv_grom <stanislav.kostenko@nixsolutions.com>
2017-04-19 11:53:40 +0300

    Change to 2017 project

--- src/Azure-File-Share/Controllers/DownloadController.cs
+++ src/Azure-File-Share/Controllers/DownloadController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.IO.Compression;
+using System.IO;
 
 namespace Azure_File_Share.Controllers
 {
@@ -20,6 +22,34 @@ namespace Azure_File_Share.Controllers
             var filepath = $"Downloads/{fileName}";
             byte[] fileBytes = System.IO.File.ReadAllBytes(filepath);
             return File(fileBytes, "application/x-msdownload", fileName);
+        }
+
+        public FileResult DownloadMultipleFiles(string fileUrl)
+        {
+            // get file names by fileurl
+            var fileName = $"img.jpg";
+            var fileName2 = $"img2.jpg";
+            string[] files = new string[] { fileName, fileName2 };
+            byte[] compressedBytes;
+
+            using (var outStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(outStream, ZipArchiveMode.Create, true))
+                {
+                    foreach (string fileSingle in files)
+                    {
+                        var fileInArchive = archive.CreateEntry(fileSingle, CompressionLevel.Optimal);
+                        byte[] fileBytes = System.IO.File.ReadAllBytes($"Downloads/{fileSingle}");
+                        using (var entryStream = fileInArchive.Open())
+                        using (var fileToCompressStream = new MemoryStream(fileBytes))
+                        {
+                            fileToCompressStream.CopyTo(entryStream);
+                        }
+                    }
+                }
+                compressedBytes = outStream.ToArray();
+            }
+            return File(compressedBytes, "application/x-msdownload", "zipName.zip");
         }
     }
 }
